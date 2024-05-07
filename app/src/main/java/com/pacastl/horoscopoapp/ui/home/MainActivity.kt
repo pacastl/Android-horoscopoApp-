0729c02f226984b6a0379f4dffc98ad7cf0836c5
pablo <pablocl09@gmail.com>
2024-05-07 14:06:48 +0200

    Basic config

--- app/src/main/java/com/pacastl/horoscopoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/pacastl/horoscopoapp/ui/home/MainActivity.kt
@@ -5,22 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.pacastl.horoscopoapp.R
 import com.pacastl.horoscopoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
-
+//    Clase padre que gestiona la navegación del navigation
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding= ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.root)
+        initUI()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+    }
+    private fun initUI(){
+        val navHost =supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+//        El controlador controla el fragmentContainerView
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
